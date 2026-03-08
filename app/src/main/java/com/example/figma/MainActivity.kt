3564eb79a39875270e15eacde6274d53416d7799
astridsf <asfa0002@stud.ek.dk>
2026-03-08 15:36:05 +0100

    commit

--- app/src/main/java/com/example/figma/MainActivity.kt
+++ app/src/main/java/com/example/figma/MainActivity.kt
@@ -1,52 +1,40 @@
 package com.example.figma
 
-import android.R.attr.top
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.figma.ui.theme.FigmaTheme
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.wrapContentWidth
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.TextField
-
-import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.HorizontalDivider
-import androidx.compose.ui.graphics.Color
-import androidx.compose.foundation.layout.Arrangement
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -78,6 +66,7 @@ fun App() {
         contentPadding = PaddingValues(horizontal = 24.dp, vertical = 24.dp)
     ) {
         item {
+
             AppImage(R.drawable.logo)
 
             Text(
@@ -208,11 +197,30 @@ fun App() {
                 horizontalArrangement = Arrangement.spacedBy(12.dp, Alignment.CenterHorizontally),
                 verticalAlignment = Alignment.CenterVertically
             ) {
+                Button(
+                    onClick = {},
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = Color(0xFFF8F9FA)
+                    )
+                ) {
+                    Text(
+                        text = "Se layout",
+                        fontSize = 20.sp,
+                        color = Color(0xFF26234F)
+                    )
+                }
 
                 Button(
                     onClick = {
                         applicationList.add(
-                            Application(fornavn, efternavn, alder, stilling, kompetencer, om)
+                            Application(
+                                fornavn,
+                                efternavn,
+                                alder.toIntOrNull() ?: 0,
+                                stilling,
+                                kompetencer,
+                                om
+                            )
                         )
                         fornavn = ""
                         efternavn = ""
@@ -235,79 +243,28 @@ fun App() {
         }
         //rendering:
         items(applicationList) { app ->
-            Column(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(top = 20.dp)
-            ) {
-                Text(
-                    text = "Din ansøgning:",
-                    fontSize = 22.sp,
-                    modifier = Modifier
-                        .padding(bottom = 15.dp),
-                    color = Color.White
-                )
-                Text(
-                    text = "Fornavn: ${app.fornavn}",
-                    fontSize = 15.sp,
-                    modifier = Modifier
-                        .padding(bottom = 15.dp),
-                    color = Color.White
-                )
-                Text(
-                    text = "Efternavn: ${app.efternavn}",
-                    fontSize = 15.sp,
-                    modifier = Modifier
-                        .padding(bottom = 15.dp),
-                    color = Color.White
-                )
-
-                Text(
-                    text = "Alder: ${app.alder}",
-                    fontSize = 15.sp,
-                    modifier = Modifier
-                        .padding(bottom = 15.dp),
-                    color = Color.White
-                )
-                Text(
-                    text = "Stilling: ${app.stilling}",
-                    fontSize = 15.sp,
-                    modifier = Modifier
-                        .padding(bottom = 15.dp),
-                    color = Color.White
-                )
-                Text(
-                    text = "Kompetencer: ${app.kompetencer}",
-                    fontSize = 15.sp,
-                    modifier = Modifier
-                        .padding(bottom = 15.dp),
-                    color = Color.White
-                )
-                Text(
-                    text = "Om: ${app.om}",
-                    fontSize = 15.sp,
-                    modifier = Modifier
-                        .padding(bottom = 15.dp),
-                    color = Color.White
-                )
-            }
+            ApplicationCard(app)
         }
     }
 }
+
 
 @Composable
 //kildehenvisning: https://developer.android.com/codelabs/basic-android-kotlin-compose-add-images?continue=https%3A%2F%2Fdeveloper.android.com%2Fcourses%2Fpathways%2Fandroid-basics-compose-unit-1-pathway-3%23codelab-https%3A%2F%2Fdeveloper.android.com%2Fcodelabs%2Fbasic-android-kotlin-compose-add-images#2
 fun AppImage(imageRes: Int) {
     val image = painterResource(imageRes)
 
-    Image(
-        painter = image,
-        contentDescription = null,
-        modifier = Modifier
-            .size(200.dp)
-            .fillMaxWidth()
-            .wrapContentWidth(Alignment.Start)
-    )
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.Start
+    ) {
+        Image(
+            painter = painterResource(imageRes),
+            contentDescription = null,
+            modifier = Modifier
+                .size(200.dp)
+        )
+    }
 }
 
 @Composable
@@ -413,6 +370,79 @@ fun LanguageButtons() {
 
     }
 }
+
+@Composable
+fun ApplicationCard(app: Application) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(top = 20.dp)
+            .background(Color.White)
+    ) {
+        Text(
+            text = "Din ansøgning:",
+            fontSize = 30.sp,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(bottom = 25.dp),
+            textAlign = TextAlign.Center,
+            color = Color(0xFF26234F)
+        )
+
+        Text(
+            text = "Fornavn: ${app.fornavn}",
+            fontSize = 20.sp,
+            modifier = Modifier.padding(bottom = 15.dp),
+            color = Color(0xFF26234F)
+        )
+
+        Text(
+            text = "Efternavn: ${app.efternavn}",
+            fontSize = 20.sp,
+            modifier = Modifier.padding(bottom = 15.dp),
+            color = Color(0xFF26234F)
+        )
+
+        Text(
+            text = "Alder: ${app.alder}",
+            fontSize = 20.sp,
+            modifier = Modifier.padding(bottom = 15.dp),
+            color = Color(0xFF26234F)
+        )
+
+        Text(
+            text = "Stilling: ${app.stilling}",
+            fontSize = 20.sp,
+            modifier = Modifier.padding(bottom = 15.dp),
+            color = Color(0xFF26234F)
+        )
+
+        Text(
+            text = "Kompetencer: ${app.kompetencer}",
+            fontSize = 20.sp,
+            modifier = Modifier.padding(bottom = 15.dp),
+            color = Color(0xFF26234F)
+        )
+
+        Text(
+            text = "Om: ${app.om}",
+            fontSize = 20.sp,
+            modifier = Modifier.padding(bottom = 15.dp),
+            color = Color(0xFF26234F)
+        )
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
 /*
 @Composable
 fun CreateApplication() {
--- app/src/main/java/com/example/figma/forsiden.kt
+++ app/src/main/java/com/example/figma/forsiden.kt
@@ -1,5 +1,6 @@
 package com.example.figma
 
+
 import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
